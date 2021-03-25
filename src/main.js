e36bce64dffcc4f3951c42ec8de71e753add1564
Kaspar Suursalu <kasparsu@gmail.com>
2021-03-25 14:55:24 +0200

    add component and todo list example

--- src/main.js
+++ src/main.js
@@ -1,19 +1,9 @@
-console.log("aaa");
 import Vue from 'vue';
 
+import App from './App.vue';
+
+Vue.component('app', App);
+
 let app = new Vue ({
-    el: '#app',
-    data: {
-        message: '',
-        items: []
-    },
-    methods: {
-        addItem(){
-            // '     aaaaa   ' => 'aaaaa' '       ' => ''
-            if(this.message.trim() !== '') {
-                this.items.push(this.message);
-                this.message = '';
-            }
-        }
-    }
+    el: '#app'
 });
