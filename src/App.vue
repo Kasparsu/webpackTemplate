0c0bdc52d6a4bfca3d8ab2416e1dbe61cc472ad9
Kaspar Suursalu <kasparsu@gmail.com>
2021-03-25 15:58:38 +0200

    add bulma

--- src/App.vue
+++ src/App.vue
@@ -1,45 +1,17 @@
 <template>
-    <div>
-        <input type="text" v-model="message" @keypress.enter="addItem"><button @click="addItem">Add</button>
-        <item-list title="All Items" :items="items"></item-list>
-        <item-list title="Done Items" :items="doneItems"></item-list>
-        <item-list title="To Do Items" :items="toDoItems"></item-list>
+    <div class="container">
+        <section class="section">
+            <button class="button is-primary">Hello</button>
+        </section>
     </div>
 </template>
 
 <script>
-    import ItemList from "./components/ItemList";
     export default {
         name: "App",
-        components: {ItemList},
         data(){
             return {
-                message: '',
-                items: [
-                    {name:'Leib', isDone: false, id: 0},
-                    {name:'Sai', isDone: true, id: 1},
-                    {name:'Piim', isDone: false, id: 2},
-                ]
-            }
-        },
-        methods: {
-            addItem(){
-                if(this.message.trim() !== '') {
-                    this.items.push({name: this.message, isDone: false, id: this.items.length});
-                    this.message = '';
-                }
-            }
-        },
-        computed: {
-            reversedMessage(){
-                // "message" => ["m", "e" ....]
-                return this.message.split('').reverse().join('');
-            },
-            doneItems(){
-                return this.items.filter(item => item.isDone);
-            },
-            toDoItems() {
-                return this.items.filter(item => !item.isDone);
+
             }
         }
     }
