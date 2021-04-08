15d27f5daa18ee2b778a62d7cb412b54f9ecf2a5
Kaspar Suursalu <kasparsu@gmail.com>
2021-04-08 11:08:10 +0300

    add reusable modal component

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,56 @@
 <template>
     <div class="container">
         <section class="section">
-            <button class="button is-primary" @click="modalActive=true">Hello</button>
+            <button class="button is-primary" @click="modalActive=true">Pic1</button>
+            <button class="button is-primary" @click="modal2Active=true">Pic2</button>
         </section>
+        <modal :active="modalActive" @close="modalActive=false">
+            <p class="image is-4by3">
+                <img src="https://placekitten.com/1280/960" alt="">
+            </p>
+        </modal>
+        <modal :active="modal2Active" @close="modal2Active=false">
+            <div class="card">
+                <div class="card-image">
+                    <figure class="image is-4by3">
+                        <img src="https://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image">
+                    </figure>
+                </div>
+                <div class="card-content">
+                    <div class="media">
+                        <div class="media-left">
+                            <figure class="image is-48x48">
+                                <img src="https://bulma.io/images/placeholders/96x96.png" alt="Placeholder image">
+                            </figure>
+                        </div>
+                        <div class="media-content">
+                            <p class="title is-4">John Smith</p>
+                            <p class="subtitle is-6">@johnsmith</p>
+                        </div>
+                    </div>
 
-        <div class="modal is-active" v-if="modalActive">
-            <div class="modal-background" @click="modalActive=false"></div>
-            <div class="modal-content">
-                <p class="image is-4by3">
-                    <img src="https://bulma.io/images/placeholders/1280x960.png" alt="">
-                </p>
+                    <div class="content">
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                        Phasellus nec iaculis mauris. <a>@bulmaio</a>.
+                        <a href="#">#css</a> <a href="#">#responsive</a>
+                        <br>
+                        <time datetime="2016-1-1">11:09 PM - 1 Jan 2016</time>
+                    </div>
+                </div>
             </div>
-            <button class="modal-close is-large" aria-label="close" @click="modalActive=false"></button>
-        </div>
-
+        </modal>
     </div>
 </template>
 
 <script>
+    import Modal from "./components/Modal";
     export default {
         name: "App",
+        components: {Modal},
         data(){
             return {
-                modalActive: false
+                modalActive: false,
+                modal2Active: false,
             }
         }
     }
---
+++ src/components/Modal.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="modal is-active" v-if="active">
+        <div class="modal-background" @click="$emit('close')"></div>
+        <div class="modal-content">
+            <slot></slot>
+        </div>
+        <button class="modal-close is-large" aria-label="close" @click="$emit('close')"></button>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Modal",
+        props: ['active']
+    }
+</script>
+
+<style scoped>
+
+</style>
