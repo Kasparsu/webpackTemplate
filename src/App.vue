5450e1f5ecc2663de724c160f051ac0b79a36066
Kaspar Suursalu <kasparsu@gmail.com>
2021-04-08 12:49:44 +0300

    add simple pagination

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,24 @@
 <template>
     <div class="container">
         <section class="section">
-            
+            <button class="button" @click="prev" :disabled="!info.prev">Prev</button>
+            <button class="button is-pulled-right" @click="next" :disabled="!info.next">Next</button>
+
+            <div class="columns is-multiline mt-3">
+                <div class="column is-one-fifth" v-for="character in characters" :key="character.id">
+                    <card :character="character"></card>
+                </div>
+            </div>
         </section>
     </div>
 </template>
 
 <script>
     import axios from 'axios';
+    import Card from "./components/Card";
     export default {
         name: "App",
+        components: {Card},
         created(){
             axios.get('https://rickandmortyapi.com/api/character').then(response => {
                 this.info = response.data.info;
@@ -18,8 +27,25 @@
         },
         data(){
             return {
-                info: null,
+                info: {
+                    prev: null,
+                    next: null
+                },
                 characters: []
+            }
+        },
+        methods: {
+            prev(){
+                axios.get(this.info.prev).then(response => {
+                    this.info = response.data.info;
+                    this.characters = response.data.results;
+                });
+            },
+            next(){
+                axios.get(this.info.next).then(response => {
+                    this.info = response.data.info;
+                    this.characters = response.data.results;
+                });
             }
         }
     }
---
+++ src/components/Card.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="card">
+        <header class="card-header">
+            <p class="card-header-title">
+                {{character.name}}
+            </p>
+        </header>
+        <div class="card-image">
+            <figure class="image is-4by3">
+                <img :src="character.image" alt="Placeholder image">
+            </figure>
+        </div>
+        <div class="card-content">
+            <div class="content">
+                <ul>
+                    <li><b>Species:</b> {{character.species}} </li>
+                    <li><b>Origin:</b> {{character.origin.name}} </li>
+                    <li><b>Status:</b> {{character.status}} </li>
+                    <li><b>Gender:</b> {{character.gender}} </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Card",
+        props: ['character']
+    }
+</script>
+
+<style scoped>
+
+</style>
